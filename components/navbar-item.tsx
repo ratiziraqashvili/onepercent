729f5ab85b0a84a14515caff9efb6572fdef3bc1
Rati <rziraqashvili@gmail.com>
2024-03-28 17:09:10 +0400

    finished navbar for desktop

--- components/navbar-item.tsx
+++ components/navbar-item.tsx
@@ -1,20 +1,30 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const NavbarItem = () => {
+  const pathname = usePathname();
+
   const routes = [
     {
       label: "მთავარი",
       href: "/",
+      isActive: pathname === "/",
     },
     {
       label: "სრული კოლექცია",
       href: "/collections/all",
+      isActive: pathname === "/collections/all",
     },
   ];
 
   return (
     <>
-      <div className="flex gap-7">
+      <div className="flex gap-7 items-center">
         <div>
           {/* <Image
                      src={}
@@ -24,11 +34,31 @@ export const NavbarItem = () => {
         </div>
         <div className="flex gap-4">
           {routes.map((route) => (
-            <ul key={route.href}>{route.label}</ul>
+            <Link
+              href={route.href}
+              className={cn(
+                "text-sm cursor-pointer text-muted-foreground hover:text-black border-b border-white hover:border-black",
+                route.isActive && "text-black border-black hover:border-b-2"
+              )}
+              key={route.href}
+            >
+              {route.label}
+            </Link>
           ))}
         </div>
       </div>
-      <div></div>
+      <div className="flex gap-5">
+        <Search
+          strokeWidth={1}
+          className="size-6 hover:scale-105 cursor-pointer"
+        />
+        <Link href="/cart">
+          <ShoppingBag
+            strokeWidth={1}
+            className="size-6 hover:scale-105 cursor-pointer"
+          />
+        </Link>
+      </div>
     </>
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
-import { NavbarItem } from "./navbar-item"
+import { NavbarItem } from "./navbar-item";
 
 export const Navbar = () => {
-    return (
-        <nav className="h-[7.5rem] lg:h-36 max-w-[68rem] mx-auto flex items-center">
-            <NavbarItem />
-        </nav>
-    )
-}
+  return (
+    <nav className="h-[7.5rem] lg:h-36 max-w-[68rem] mx-auto flex items-center justify-between px-14 xl:px-0">
+      <NavbarItem />
+    </nav>
+  );
+};
