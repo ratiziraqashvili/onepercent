da636601138f409238860ef6052fd30d8681c78a
Rati <rziraqashvili@gmail.com>
2024-04-04 17:18:42 +0400

    correctly getting products based on collection name

---
+++ app/(routes)/collections/[collection]/page.tsx
@@ -0,0 +1,44 @@
+import { Container } from "@/components/container";
+import { ProductCard } from "@/components/product-card";
+import { client } from "@/sanity/lib/client";
+import { Product } from "@/types";
+import { groq } from "next-sanity";
+
+const CollectionPage = async ({
+  params,
+}: {
+  params: { collection: string };
+}) => {
+  const decodedCollectionName = decodeURIComponent(params.collection).replace(
+    /-/g,
+    " "
+  );
+
+  const products = await client.fetch<Product[]>(
+    groq`*[_type == "product"] {
+            _id,
+            name,
+            images,
+            price,
+            currency,
+            "categories": categories[]->{
+                _id,
+                name
+              },
+        }`
+  );
+
+  const filteredProducts = products.filter((product) =>
+    product.categories.some(
+      (category) => category.name === decodedCollectionName
+    )
+  );
+
+  return (
+    <Container>
+      <ProductCard products={filteredProducts} />
+    </Container>
+  );
+};
+
+export default CollectionPage;
