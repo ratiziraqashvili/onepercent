59e50146af2a82d3e18b89e6540c7d768d62e50d
Rati <rziraqashvili@gmail.com>
2024-04-05 15:37:52 +0400

    working on fetching products

--- components/modals/search-modal.tsx
+++ components/modals/search-modal.tsx
@@ -1,13 +1,13 @@
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import { Dialog, DialogClose, DialogContent } from "../ui/dialog";
-import { Search, X } from "lucide-react";
+import { MoveRight, Search, X } from "lucide-react";
 import { Input } from "../ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { cn } from "@/lib/utils";
-import { Button } from "../ui/button";
 
 export const SearchModal = () => {
   const [inputValue, setInputValue] = useState("");
+  const [isInputFocused, setIsInputFocused] = useState(false);
 
   const onInputChange = (event: any) => {
     setInputValue(event.target.value);
@@ -15,6 +15,14 @@ export const SearchModal = () => {
 
   const onInputClear = () => {
     setInputValue("");
+  };
+
+  const onInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const onInputBlur = () => {
+    setIsInputFocused(false);
   };
 
   return (
@@ -26,11 +34,13 @@ export const SearchModal = () => {
         />
       </DialogTrigger>
       <DialogContent className="h-[7.5rem] lg:h-36 flex justify-center items-center gap-5 md:gap-2">
-        <div className="md:w-[50%] w-full relative flex items-center">
+        <div className="xl:w-[50%] sm:w-[75%] w-full relative flex items-center">
           <Input
             className="w-full mx-auto rounded-none placeholder:tracking-widest peer pl-4 pr-8"
             value={inputValue}
             onChange={onInputChange}
+            onFocus={onInputFocus}
+            onBlur={onInputBlur}
           />
           <span
             className={cn(
@@ -53,6 +63,23 @@ export const SearchModal = () => {
                 role="button"
               >
                 <X className="size-4 text-muted-foreground" strokeWidth={1} />
+              </div>
+            </div>
+          )}
+          {inputValue && isInputFocused && (
+            <div className="sm:w-[101.3%] w-screen overflow-y-auto absolute top-20 sm:top-11 sm:-left-1 -left-6 flex">
+              <div className="flex-1">
+                {/* <SearchSuggestions /> */}
+                {/* <SearchProducts /> */}
+              </div>
+              <div className="flex justify-between items-center group w-full cursor-pointer hover:bg-primary/5 py-3 px-3">
+                <span className="text-sm tracking-wide">
+                  იძებნება "{inputValue}"
+                </span>
+                <MoveRight
+                  strokeWidth={1}
+                  className="size-4 group-hover:translate-x-1 group-hover:scale-110 transition"
+                />
               </div>
             </div>
           )}
